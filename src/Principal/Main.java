90fd1c0bd872a7a8ea96dc9be49c36cc753e82c2
itachi <itachi@192.168.1.14>
2021-03-28 18:27:22 +0200

    esta?

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -2,9 +2,56 @@ package Principal;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String nombre;
+	private String dni;
+	private int tlfn;
+	private boolean conectado;
+	
 
+	public Main() {
+	
+		nombre=" ";
+		dni=" ";
+		tlfn=0;
+		conectado=false;
+	}	
+	public Main(String nombre,String dni,int tlfn,boolean conectado) {
+		
+		this.nombre=nombre;
+		this.dni=dni;
+		this.tlfn=tlfn;
+		this.conectado=conectado;
 	}
-
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public int getTlfn() {
+		return tlfn;
+	}
+	public void setTlfn(int tlfn) {
+		this.tlfn = tlfn;
+	}
+	public boolean isConectado() {
+		return conectado;
+	}
+	public void setConectado(boolean conectado) {
+		this.conectado = conectado;
+	}
+	public String toString() {
+		
+		return "- Nombre: " + getNombre() + "\n"
+			 + "- DNI: " + getDni() + "\n"
+			 + "- Telefono: " + getTlfn() + "\n"
+			 + "- Conectado: " + isConectado();
+	}
 }
+
