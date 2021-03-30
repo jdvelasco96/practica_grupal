5751706c338dd4ce176ab1f42839e15146a7f400
jdvelasco96 <78481663+jdvelasco96@users.noreply.github.com>
2021-03-30 12:51:39 +0200

    Codigo realizado sin pruebas

--- src/Utiles/LecturaFichero.java
+++ src/Utiles/LecturaFichero.java
@@ -8,10 +8,11 @@ public class LecturaFichero {
 	// al metodo le pasamos un objeto de la clase file (una direccion) entonces nos mete en un array list cada linea del
 	// archivo que lee y lo devuelve a la llamada donde tendremos que tratar al informacion como necesitemos.
 
-	public static ArrayList<String> leerFichero(File fichero) throws Exception {
+	public static ArrayList<String> leerFichero(String dir) throws Exception {
+		
 		ArrayList<String> datos=new ArrayList<String>(); 
 		BufferedReader br=null;
-		br= new BufferedReader(new FileReader(fichero));
+		br= new BufferedReader(new FileReader(dir));
 						
 		while (br.ready()) {
 			datos.add(br.readLine().trim());
