4d74f5c21b9ff421bc75a953d1be936b0ecac130
Jorge <jpardopacheco@ceinmark.net>
2021-03-30 09:18:04 +0200

    depurado copia seguridad

--- src/Principal/EscrituraFichero.java
+++ src/Principal/EscrituraFichero.java
@@ -6,35 +6,21 @@ import java.util.Scanner;
 
 
 public class EscrituraFichero {
-	public static void main(String args[]) {
-		
-		ArrayList<String>pruebas=new ArrayList<String>();
-		pruebas.add("Hola mundo");
-		pruebas.add("que tal");
-		pruebas.add("pruebas ed");
-		pruebas.add("pruebas ed 2222");
-		pruebas.add("pruebas ed 2222 ceinmark");
-		try {
-			escribirFichero(pruebas);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+	// metodo para la copia de seguridad, se le pasa un arraylist con toda la informacion que se quiere guardar
+	// y nos pide un directorio para guardar el archivo 
 	public static void escribirFichero(ArrayList<String> a) throws Exception {
 		Scanner sc= new Scanner(System.in);
 		File directorio=null;
 		System.out.println("Escribe el directorio donde deseas guardar los datos");
 		
 		directorio = new File(sc.next().replace('/','\\'));
-		System.out.println(directorio.getAbsolutePath());
-				
 		BufferedWriter bw=new BufferedWriter(new FileWriter(directorio, true));
 				
 		for (String z : a) {
 			bw.write(z);
 			bw.newLine();
 		}
+		System.out.println("Copia de seguridad realizada con exito en: "+directorio.getAbsolutePath());
 		bw.close();
 		sc.close();
 	}
